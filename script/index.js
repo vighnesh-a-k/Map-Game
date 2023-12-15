366347d996533422e9da725c9cd8b768c404cfcb
Athul V Nair <athul.nair@experionglobal.local>
2023-12-15 12:33:42 +0530

    feat: added a function to play audio on starting the game

--- script/index.js
+++ script/index.js
@@ -1,35 +1,23 @@
-function start() {
-
-
-  let nameStore = document.getElementById("firstName").value;
-  let noOfQuestions = document.getElementById("number").value;
-
-
-
-    
-    
-  sessionStorage.setItem("playerName", nameStore);
-  sessionStorage.setItem("noOfQuestion", noOfQuestions);
+function playAudio(audio) {
+    return new Promise((resolve) => {
+      audio.play();
+      audio.onended = resolve;
+    });
+  }
+  async function start() {
+    let nameStore = document.getElementById("firstName").value;
+    let noOfQuestions = document.getElementById("number").value;
   
-  //    if (nameStore == "") {
-  //   window.alert("Type first name");
-  //   return;
-  // } else {
-  //   //this code needs fixing link is not reached
-  //   window.location.replace("./game.html");
-  //   console.log("reached")
-  // }
-
-
-  if (nameStore == "") {
-    window.alert("Type first name");
-  } else {
-    window.location.assign("./game.html");
+    sessionStorage.setItem("playerName", nameStore);
+    sessionStorage.setItem("noOfQuestion", noOfQuestions);
+  
+    if (nameStore == "") {
+      window.alert("Type first name");
+    } else {
+      let audio = new Audio("assets/start-13691.mp3");
+      audio.loop = false;
+      await playAudio(audio);
+      window.location.assign("./game.html");
+    }
   }
   
-  
-  }
-  
-  // window.onload = function(){
-  //     document.getElementById("intro-music").play();
-  // }
